cfe7020d9130985b5eb490df413796eda05c8434
Oznerol <43137813+lorenzo-bts-sio@users.noreply.github.com>
2022-04-16 17:51:57 +0200

    Add files via upload

--- Controllers/Agents.java
+++ Controllers/Agents.java
@@ -9,7 +9,24 @@ public class Agents{
 
         
         BufferedWriter writer = new BufferedWriter(new FileWriter(path_output, true ));
-       if (name_div =="poste")
+       
+       if (name_div=="nom")
+       {
+        writer.write("<!DOCTYPE html> \n"
+           + "<html lang='fr'>\n"
+           + "<head>\n"
+           + "<meta charset='utf-8'>\n"
+           + "<meta http-equiv='X-UA-Compatible' content='IE=edge'>\n"
+           + "<meta name='viewport' content='width=device-width, initial-scale=1'>\n"
+           + "<title>"+ficher_agent+"</title>\n"      
+           + "<link rel='stylesheet' href='styles.css'>\n"
+           + "</head>\n"
+           + "<body>\n"
+           + "<button onclick='window.location.href='/'>Retour</button>\n"
+
+            );
+       }
+        else if (name_div =="poste")
        {
             writer.write("<h2> Poste : </h2> <div Class="+name_div+"> <h"+number+">"+ficher_agent+"</h"+number+"> </div> <h2> Liste du matériel pris  : </h2> \n");
        }
--- Controllers/Index.java
+++ Controllers/Index.java
@@ -10,7 +10,19 @@ public class Index  {
 public  void  generate_index(Object name_agent , String path_output) throws IOException {
 
     BufferedWriter writer = new BufferedWriter(new FileWriter(path_output, true));
-       
+    
+    writer.write("<!DOCTYPE html>"
+           + "<html lang='fr'>"
+           + "<head>"
+           + "<meta charset='utf-8'>"
+           + "<meta http-equiv='X-UA-Compatible' content='IE=edge'>"
+           + "<meta name='viewport' content='width=device-width, initial-scale=1'>"
+           + "<title>index</title>"      
+           + "<link rel='stylesheet' href='styles.css'>"
+           + "</head>"
+           + "<body>"
+          
+            );
     writer.write("<li><a href=/"+name_agent +".html >"  + name_agent + "</a> </li> \n");
     writer.write("</ul>");
     writer.flush();
